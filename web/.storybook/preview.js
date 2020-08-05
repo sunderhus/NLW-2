8fd47eba9f4e8a28c33fad92a2e68f67bf25cb36
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-08-05 18:44:31 -0300

    fix: remove storybook frmo this project.

--- web/.storybook/preview.js
+++
@@ -1,6 +0,0 @@
-import { withInfo } from "@storybook/addon-info";
-import { addDecorator } from "@storybook/react";
-import centered from "@storybook/addon-centered/react";
-
-addDecorator(withInfo);
-addDecorator(centered);
